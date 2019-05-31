a135589927343db705548f0bbd8998a7e404bf47
Michael Patel <michael.patel94@gmail.com>
2019-05-31 13:58:52 -0400

    Can read mp4 successfully

--- temp/hitp_mp4.py
+++ temp/hitp_mp4.py
@@ -25,3 +25,16 @@ input_file = os.path.join(os.getcwd(), "hitp.mp4")
 
 # create VideoCapture object by reading from video file
 vc = cv2.VideoCapture(input_file)
+
+while vc.isOpened():
+    ret, frame = vc.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vc.release()
+cv2.destroyAllWindows()
+
